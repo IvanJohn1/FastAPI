3c896766b20c1f5dd209a54503bee207715a1ced
IvanJohn1 <118132427+IvanJohn1@users.noreply.github.com>
2024-05-16 03:04:50 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -27,5 +27,5 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", port=5000, log_level="info")
+# if __name__ == "__main__":
+#     uvicorn.run("main:app", port=5000, log_level="info")
